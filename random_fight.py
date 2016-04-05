af85b2c0c632daa00df39fc13af6823caafe1f58
Mark Jenkins <mark@markjenkins.ca>
2016-04-05 01:28:28 -0500

    proposed fights now recorded

--- random_fight.py
+++ random_fight.py
@@ -4,8 +4,11 @@ import boto3
 
 from random import sample
 
+from uuid import uuid4
+
 dynamodb = boto3.resource('dynamodb')
 monster_table = dynamodb.Table('monsters')
+fights_table = dynamodb.Table('fights')
 
 # the plan is to not do this everytime a fight is requested, but to cache
 # along side the database connection
@@ -17,6 +20,16 @@ p1, p2 =  sample(all_monsters, 2)
 
 def print_combantant(c):
     print( "%(size)d %(size_animal)s-sized %(base_animal)s" % c)
+
+fight_uuid = uuid4()
+
+fights_table.put_item(Item={
+    'fight_uuid': fight_uuid.hex, 
+    'c1': p1['animal_id'],
+    'c2': p2['animal_id'],
+})
+
+print( fight_uuid.hex )
 
 print_combantant(p1)
 
